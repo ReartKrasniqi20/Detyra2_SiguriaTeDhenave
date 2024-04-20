1f8a4a7757a082ae1036042c06f7201d41af0983
ReartKr <147925754+ReartKrasniqi20@users.noreply.github.com>
2024-04-20 15:32:07 -0700

    Update TurninGrilleCipherEncryptTest.java

--- TurninGrilleCipher/TurninGrilleCipherEncryptTest.java
+++ TurninGrilleCipher/TurninGrilleCipherEncryptTest.java
@@ -1,15 +1,39 @@
-package TurninGrilleCipher;
-
-public class TurninGrilleCipherEncryptTest {
+import java.util.Scanner;
+public class Main {
 
     public static void main(String[] args) {
-        String plaintext = "HELLO";
-        char[][] grille = {
-            {'X', 'X'},
-            {'X', '.'}
-        };
-        
-        String encryptedText = TurninGrilleCipherEncrypt.encrypt(plaintext, grille);
-        System.out.println("Encrypted Text: " + encryptedText);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Shenoni nje tekst:");
+        String text = scanner.nextLine();
+        if (text.length() < 36) {
+            while (text.length() < 36) {
+                text = text + "x";
+            }
+        }
+        if (text.length() != 36) {
+            text = fleissner_decrypt.substring(text, 0, 36);
+            System.out.println("Teksti shume i gjate, teksti i cili do enkriptohet: " + text);
+        }
+
+        char[][] out = fleissner_encrypt.encrypt(text);
+        for (int line = 0; line < out.length; line++) {
+
+            for (int column = 0; column < out[line].length; column++) {
+                System.out.print(out[line][column] + " ");
+            }
+
+            System.out.println();
+        }
+
+
+        System.out.println("\n");
+        for (int line = 0; line < out.length; line++) {
+            for (int column = 0; column < out[line].length; column++) {
+                System.out.print(out[line][column] + "");
+            }
+        }
+        System.out.println();
+
     }
 }
